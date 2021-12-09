a50ceddcd014f5c1568c9facdd78531a5f2d7639
pcoll21 <94703615+pcoll21@users.noreply.github.com>
2021-12-09 17:27:12 -0500

    Add files via upload

--- final.py
+++ final.py
@@ -23,38 +23,43 @@ def totalFunction() :
     
     dbcursor.execute("Set search_path to final")
     
-    dbcursor.execute("CREATE TEMP TABLE t1 as SELECT ship_id, ship_name, ship_weight FROM ships group by ship_id")
+    dbcursor.execute("Create temp table t1 as select ship_id, shipment_date, sum(num_containers) as total_num_cont, sum(cont_weight) as total_cont_weight, sum(value * num_containers) as total_value from cargo natural join hold group by shipment_date, ship_id")
     
     
-    dbcursor.execute("CREATE TEMP TABLE t2 as SELECT ship_id, ship_name, sum(cont_weight * num_containers) as total_cont_weight, shipment_date FROM t1 NATURAL JOIN hold")
+    dbcursor.execute("select ship_id, ship_weight, shipment_date, total_num_cont, total_cont_weight, total_value from t1 natural join ships")
     
 
-    dbcursor.execute("Select ship_id, ship_name, ship_weight, num_containers, cont_weight, value, sum(ship_weight + (cont_weight * num_containers)) AS weight From t1 natural join t2")
-    
+    dbcursor.execute("select ship_id, shipment_date, sum(ship_weight + (total_num_cont * total_cont_weight)) as total_weight, total_value from t1 natural join ships group by ship_id, shipment_date, total_value;")
     recordList = dbcursor.fetchall() 
     
     
 	# print out the formatted version of the report	
-    print("TOTAL WEIGHT AND VALUE THROUGH PANAMA CANAL")
-    print("                                    2011-10-13                               ")
+    print("           TOTAL WEIGHT AND VALUE THROUGH PANAMA CANAL")
+    print("                         2011-10-13                              ")
     print()
 	
-    print("SHIP#  SHIP-NAME SHIP_WEIGHT NUM_CONTAINERS CONT_WEIGHT VALUE  TOTAL_WEIGHT")
-    print("-----  --------- ----------- -------------- ------------ -----  ------------")
+    print("SHIP#   SHIPMENT_DATE TOTAL_WEIGHT TOTAL VALUE")
+    print("---------------------------------------------------------")
 
 	# select the records to be displayed
     for record in recordList :
-	    print("%5d    %-10d   %-5.2f  %-4d   %-5.2f  %-5d   %-6.2f" % record)
+	    print("%5d   %10s  %-8d %-8d" % record)
 		
-    print("----------------------------------------------------------------------------")
+    print("-----------------------------------------------------")
 	
 	# print out the total weight and values
-    total_weight = dbcursor.execute("Select sum(weight) from t1")
+    dbcursor.execute("create temp table t3 as select ship_id, shipment_date, sum(ship_weight + (total_num_cont * total_cont_weight)) as total_weight, total_value from t1 natural join ships group by ship_id, shipment_date, total_value;")
+    
+    dbcursor.execute("Select sum(total_weight) from t3")
+    
+    total_weight = dbcursor.fetchall()
 	
-    total_value = dbcursor.execute("Select sum(value) from t1")
+    dbcursor.execute("Select sum(total_value) from t3")
+    
+    total_value = dbcursor.fetchall()
 	
-    print("Total WEIGHT: %d" % total_weight)
-    print("Total VALUE:  %d" % total_value)
+    print("Total WEIGHT: %d" % total_weight[0])
+    print("Total VALUE:  %d" % total_value[0])
 
 totalFunction()
 
